5830ffc5363e67b4d326223a97dc482e6d9fbae6
Duc Anh <thaianh02012002@gmail.com>
2021-04-11 10:26:50 +0700

    add login and change pass FE

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from testapp.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', testapp.views.base_views)
 ]
